2ef513ef2b1609440e618ddface3fb4596e01da0
Anthony NGUYEN <anthony.ng@hotmail.fr>
2020-03-19 19:30:57 +0100

    Inital commit - start project with Spring Initializr

---
+++ src/test/java/com/example/gscrudwithvaadin/GsCrudWithVaadinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gscrudwithvaadin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsCrudWithVaadinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
